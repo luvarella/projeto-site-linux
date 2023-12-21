f6ac00b1264388d5f2cfab117b62192fe9bf2c93
balbii <93939187+balbii@users.noreply.github.com>
2023-12-21 14:42:22 -0300

    Add files via upload

---
+++ site-frontend/js/abrirfechar.js
@@ -0,0 +1,8 @@
+function deletarconta(){
+  document.getElementById('deletar').style.visibility = 'visible';
+  document.getElementById('esmaecer').style.visibility = 'visible';
+}
+function fechar(){
+  document.getElementById('deletar').style.visibility = 'hidden';
+  document.getElementById('esmaecer').style.visibility = 'hidden';
+}
