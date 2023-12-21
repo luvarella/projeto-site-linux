1f6562f18776db896253f78858ab5100ee71a18a
Jordson Galvão <105514196+JordsonZ@users.noreply.github.com>
2023-12-20 23:43:34 -0300

    Create abrirfechar.js

---
+++ site-frontend/js/abrirfechar.js
@@ -0,0 +1,8 @@
+function deletarconta(){
+  document.getElementById('denuncia').style.visibility = 'visible';
+  document.getElementById('esmaecer').style.visibility = 'visible';
+}
+function fechar(){
+  document.getElementById('denuncia').style.visibility = 'hidden';
+  document.getElementById('esmaecer').style.visibility = 'hidden';
+}
