9f64c54e5080c8eb8426e2ef382e422f6926be67
balbii <93939187+balbii@users.noreply.github.com>
2023-12-21 14:39:27 -0300

    Delete site-frontend directory

--- site-frontend/js/abrirfechar.js
+++
@@ -1,8 +0,0 @@
-function deletarconta(){
-  document.getElementById('denuncia').style.visibility = 'visible';
-  document.getElementById('esmaecer').style.visibility = 'visible';
-}
-function fechar(){
-  document.getElementById('denuncia').style.visibility = 'hidden';
-  document.getElementById('esmaecer').style.visibility = 'hidden';
-}
